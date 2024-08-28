4683b8566d9a965d473d7ff7afaabbcee927adb8
Noew777 <noepaniiagua@gmail.com>
2024-08-29 01:45:08 +0200

    v1.3: ADC first try

--- examples/adcTest/adcTest.c
+++ examples/adcTest/adcTest.c
@@ -2,13 +2,15 @@
  * 
  *            Noeww bre-metal Arduino :)
  *            
- *          8bit timer example in ATMega328
+ *            ADC example in ATMega328
  * 
  ******************************************************/
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
 void setUpIO(void);
+void setUpTimer0CTC(void);
+void setUpADC0(void);
 
 /***********************
 * MAIN FUNCTION JJJJJJ *
@@ -17,7 +19,8 @@ int main() {
   setUpIO();  // Set up all IO ports
   CLKPR =(0x80);
   CLKPR =(0x01);
-  initTimerCTC();
+  setUpTimer0CTC();
+  setUpADC0();
 
   SREG |= (0x80); // Enable global interrupts 
   
@@ -31,7 +34,7 @@ int main() {
 //
 // Init timer at CTC mode
 //
-void initTimerCTC(void){
+void setUpTimer0CTC(void){
   TCCR0A |= (0X00); // set ctc mode and none about pwm
 
   // Configurar el preescalador en 1024
@@ -51,8 +54,19 @@ void setUpIO(void){
 }
 
 //
-// ISR handler for timer0 ovf pins
+// Configure ADC 
 //
-ISR(TIMER0_OVF_vect){
-  PORTB ^= (1 << PORTB5);// Toggle pin 13
+void setUpADC0(void){
+  ADMUX |= (1 << ADLAR);  // Read just 8bits (just one register)
+  ADCSRA |= 0xE8; // Active all configs of ADC...
+  ADCSRB |= 0x04; // Active the interrupt for Timer0 overflow interrupt
 }
+
+//
+// ISR handler for ADC
+//
+ISR(ADC_vect){
+  if (ADCH < 128) 
+    PORTB ^= (1 << PORTB5);// Toggle pin 13
+}
+
